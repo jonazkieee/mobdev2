30c3e7d7f0e2d30a9475f3ca9cc085afa2c72c79
Jonas <jonas.gacayan@lorma.edu>
2026-02-24 15:46:30 +0800

    Add main function with initial todo app variables

---
+++ part5.dart
@@ -0,0 +1,13 @@
+void main() {
+  String taskText = "Buy groceries";
+  bool isDone = false;
+  final String id = "todo-001";
+  String? dueDate = null;
+  const appName = "My Focus Todo App";
+
+  print(taskText);
+  print(isDone);
+  print(id);
+  print(dueDate);
+  print(appName);
+}
